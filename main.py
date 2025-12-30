36c17f5078419ec8cd095739831b49bb4e4f3a1b
Souhail-Bel <robotboy976@gmail.com>
2025-12-30 17:27:28 +0100

    Reimu now appears

--- main.py
+++ main.py
@@ -6,27 +6,39 @@ pygame.init()
 FPS = 60
 pyClock = pygame.time.Clock()
 
-RED = (0xFF, 0, 0)
-GREEN = (0, 0xFF, 0)
-BLUE = (0, 0, 0xFF)
-BLACK = (0, 0, 0)
-WHITE = (0xFF, 0xFF, 0xFF)
+BG_COLOR = (0xC0, 0xC0, 0xC0)
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 DISPLAY_SURFACE = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-DISPLAY_SURFACE.fill(WHITE)
+DISPLAY_SURFACE.fill(BG_COLOR)
 pygame.display.set_caption("Very cool game")
 
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("res/reimu.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (400, 500)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+
 def GameLoop():
+    P1 = Player()
+
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-        DISPLAY_SURFACE.fill(WHITE)
+
+        DISPLAY_SURFACE.fill(BG_COLOR)
+        P1.draw(DISPLAY_SURFACE)
+
         pygame.display.update()
         pyClock.tick(FPS)
 
