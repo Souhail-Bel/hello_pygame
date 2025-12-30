740c43ece9af37c20ac53973f9b4555c0215eb41
Souhail-Bel <robotboy976@gmail.com>
2025-12-30 12:03:45 +0100

    Added basic pygame window

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
-import pygame
+import pygame, sys
+from pygame.locals import *
 
-def main():
-    print("Hello there")
+pygame.init()
+
+FPS = 60
+pyClock = pygame.time.Clock()
+
+RED = (0xFF, 0, 0)
+GREEN = (0, 0xFF, 0)
+BLUE = (0, 0, 0xFF)
+BLACK = (0, 0, 0)
+WHITE = (0xFF, 0xFF, 0xFF)
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+DISPLAY_SURFACE = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+DISPLAY_SURFACE.fill(WHITE)
+pygame.display.set_caption("Very cool game")
+
+
+def GameLoop():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        DISPLAY_SURFACE.fill(WHITE)
+        pygame.display.update()
+        pyClock.tick(FPS)
 
 
 if __name__ == "__main__":
-    main()
+    print("Enjoy :3")
+    GameLoop()
