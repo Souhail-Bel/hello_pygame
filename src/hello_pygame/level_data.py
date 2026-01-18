ccf33dd9430d4364d5c8217c8b38b1413d21789a
Souhail-Bel <robotboy976@gmail.com>
2026-01-18 16:12:43 +0100

    Added sample of how level scripting should be

---
+++ src/hello_pygame/level_data.py
@@ -0,0 +1,28 @@
+# Format:
+#     time: when does the enemy appear
+#     type: red/green/blue/boss
+#     pos : where does it spawn
+#     script [instructions]:
+#         move X Y SPEED
+#         pattern PATTERN ARGS..
+#         wait SECONDS
+#         die
+
+
+from hello_pygame.settings import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
+LEVEL_1 = [
+    {
+        "time": 1.0,
+        "type": "red",
+        "pos": (0, 0),
+        "scipt": [
+            "move 400 300 100",
+            "pattern aim 0.8",
+            "wait 2.0",
+            "move -50 500 200",
+            "die",
+        ],
+    }
+]
